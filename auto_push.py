7f0837ba438bbdf9b15a57ca08880039fc44c9fb
c0xffee <starcyh@gmail.com>
2021-09-19 22:58:15 +0800

    cool

--- auto_push.py
+++ auto_push.py
@@ -1,6 +1,11 @@
 import os
 
 
+def fix_github_delay():
+  fname = 'README.md'
+  with open(fname, 'a+') as f:
+    f.write(' ')
+
 cmds = '''
 git pull
 git add -A
@@ -8,6 +13,8 @@ git commit -am "cool"
 git push
 '''
 cmds = [cmd for cmd in cmds.split('\n') if cmd != '']
+
+fix_github_delay()
 
 for cmd in cmds:
   print(cmd)
